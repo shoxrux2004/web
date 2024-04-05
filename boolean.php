dda7f2685debc71aa75d93343ab72954d3935fdf
Shoxrux <shoxruxnorqoziyev2004@gmail.com>
2024-04-05 18:34:39 +0500

    If else va bolean misollar

---
+++ boolean.php
@@ -0,0 +1,83 @@
+<?php
+
+// Shoxrux Norqo'ziyev 203 - guruh.
+// Q.Abdurahimov Boolean.
+
+// 10-misol.
+// $a=7;
+// $b=5;
+// $s=($a%2!=0 and $b%2==0 or $a%2==0 and $b%2!=0);
+// echo $s;
+
+// 11-misol.
+// $a=2;
+// $b=3;
+// $s=($a%2!=0 and $b%2!=0 or $a%2==0 and $b%2==0);
+// echo $s;
+
+// 12-misol.
+// $a=4;
+// $b=8;
+// $c=1;
+// $s=($a>0 and $b>0 $c>0);
+// echo $s;
+
+// 20-misol.
+// $n=123;
+// $a=round($n%10);
+// $b=$a;
+// echo $b;
+
+// 24-misol.
+// $a=1;
+// $b=-5;
+// $c=6;
+// $d=$b*$b-4*$a*$c;
+// $s=(($a!=0 and $d==0) or ($a!=0 and $d>0));
+// echo $s;
+
+// 25-misol.
+// $x=-1;
+// $y=3;
+// $s=$x<0 and $y>0;
+// echo $s;
+
+// 26-misol.
+// $x=-1;
+// $y=3;
+// $s=$x<0 and $y<0;
+// echo $s;
+
+// 27-misol.
+// $x=-1;
+// $y=3;
+// $s=($x<0 and $y>0) or ($x<0 and $y<0);
+// echo $s;
+
+// 28-misol.
+// $x=-1;
+// $y=3;
+// $s=($x>0 and $y>0) or ($x<0 and $y<0);
+// echo $s;
+
+// 30-misol.
+// $a=7;
+// $b=5;
+// $c=4;
+// $s=($a==$b and $a==$c and $b==$c);
+// echo $s;
+
+// 33-misol.
+// $a=2;
+// $b=5;
+// $c=6;
+// $s=($a+$b>$c and $a-$b>$c and $a+$c>$b and $a-$c<$b and $b-$c<$a);
+// echo $s;
+
+// 34-misol.
+// $x=7;
+// $y=4;
+// $s=($x%2==0 and $y%2==1 or $x%2==1 and $y%2==0);
+// echo $s;
+
+?>
---
+++ ifelse.php
@@ -0,0 +1,149 @@
+
+<?php
+// Norqo'ziyev Shoxrux 203-guruh talabasi.
+// Qudrat Abdurahimov Shart operatori.
+
+// 10-misol.
+// $a=3;
+// $b=4;
+// if ($a == $b) {
+//   echo 0;
+// } else {
+//   echo $a+$b;
+// }
+
+// 11-misol.
+// $a=12;
+// $b=4;
+// if ($a == $b) {
+//   echo 0;
+// } elseif ($a>$b) {
+//   echo $a;
+// }else {
+//     echo $b;
+// }
+  
+// 12-misol.
+// $a=4;
+// $b=2;
+// $c=7;
+// if ($a<$b and $a<$c) {
+//     echo $a;
+//   } elseif ($b<$a and $b<$c) {
+//     echo $b;
+//   }  else {
+//     echo $c;
+//   }
+
+// 13-misol.
+// $a=3;
+// $b=5;
+// $c=9;
+// if ($a<$b and $a>$c or $a>$b and $a<$c) {
+//     echo $a;
+//   } elseif ($b<$a and $b>$c or $b>$a and $b<$c) {
+//     echo $b;
+//   }  else {
+//     echo $c;
+//   }
+
+// 14-misol.
+// $a=4;
+// $b=2;
+// $c=7;
+// if ($a<$b and $a<$c) {
+//     echo $a;
+//     if($b>$c){
+//         echo $b;
+//     }
+//     else{
+//         echo $c;
+//     }
+//   } elseif ($b<$a and $b<$c) {
+//     echo $b;
+//     if($a>$c){
+//         echo $a;
+//     }
+//     else{
+//         echo $c;
+//     }
+//   }  else {
+//     echo $c;
+//     if($a>$b){
+//         echo $a;
+//     }
+//     else{
+//         echo $b;
+//     }
+//   }
+
+// 15-misol.
+// $a=4;
+// $b=2;
+// $c=7;
+// if ($a<$b and $a<$c) {
+//     echo $b+$c;
+//   } elseif ($b<$a and $b<$c) {
+//     echo $a+$c;
+//   }  else {
+//     echo $a+$b;
+//   }
+
+// 20-misol.
+// $a=4;
+// $b=2;
+// $c=7;
+// if (($a-$b)>($a-$c)) {
+//     echo $b;
+//   } else {
+//     echo $c;
+//   }
+
+// 21-misol.
+// $x=5;
+// $b=2;
+// if ($x==0 and $y==0) {
+//     echo 0;
+//   } elseif ($x>0 and $y==0) {
+//     echo 1;
+//   }
+//   elseif ($a==0 and $b>0) {
+//     echo 2;
+//   }
+//     else {
+//     echo 3;
+//   }
+
+// 22-misol.
+// $x=5;
+// $b=2;
+// if ($x>0 and $y>0) {
+//     echo 1;
+//   } elseif ($x>0 and $y<0) {
+//     echo 4;
+//   }
+//   elseif ($x<0 and $y>0) {
+//     echo 3;
+//   }
+//     else {
+//     echo 2;
+//   }
+
+// 23-misol.
+// $x=7;
+// if ($x>0) {
+//     echo 2*sin($x);
+//   } else {
+//     echo $x-6;
+//   }
+
+
+// 28-misol.
+// $n=2024;
+// if (($n%4==0 and $n%100!=0) or ($n%400==0)) {
+//     echo "Kabisa yil";
+//   } else {
+//     echo "Kabisa yil emas";
+//   }
+
+?>
